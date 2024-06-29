07abb095e71856fec13f7c80fc2b5edcf965a45e
hamsar4j <hamsarajs8@gmail.com>
2024-06-30 02:39:52 +0800

    fix: fixed missing openapi schema

--- CarparkInfo/Program.cs
+++ CarparkInfo/Program.cs
@@ -21,6 +21,7 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
+// get all carparks
 app.MapGet("/allcarparks", async (CarparkInfoContext dbContext) =>
     {
         var CsvProcessor = new Csv2List();
@@ -34,14 +35,18 @@ app.MapGet("/allcarparks", async (CarparkInfoContext dbContext) =>
     .WithName("GetAllCarparks")
     .WithOpenApi();
 
+// get carparks that have free parking
 app.MapGet("/freeparking", async (CarparkInfoContext dbContext) =>
-{
-    var freeParkingRecords = await dbContext.CarparkInfos
-        .Where(x => x.FreeParking != "NO")
-        .ToListAsync();
-    return freeParkingRecords;
-});
+    {
+        var freeParkingRecords = await dbContext.CarparkInfos
+            .Where(x => x.FreeParking != "NO")
+            .ToListAsync();
+        return freeParkingRecords;
+    })
+    .WithName("GetFreeParking")
+    .WithOpenApi();
 
+// get carparks that have night parking
 app.MapGet("/nightparking", async (CarparkInfoContext dbContext) =>
     {
         var nightParkingRecords = await dbContext.CarparkInfos
@@ -54,22 +59,28 @@ app.MapGet("/nightparking", async (CarparkInfoContext dbContext) =>
 
 var requestMinGantryHeight = 3;
 
+// get carparks that are above the minimum gantry height
 app.MapGet("/gantryheight", async (CarparkInfoContext dbContext) =>
-{
-    var gantryHeightRecords = await dbContext.CarparkInfos
-        .Where(x => x.GantryHeight >= requestMinGantryHeight)
-        .ToListAsync();
-    return gantryHeightRecords;
-});
+    {
+        var gantryHeightRecords = await dbContext.CarparkInfos
+            .Where(x => x.GantryHeight >= requestMinGantryHeight)
+            .ToListAsync();
+        return gantryHeightRecords;
+    })
+    .WithName("GetCarparksAboveMinGantryHeight")
+    .WithOpenApi();
 
 var requestCarparkNumber = "AM43";
 
+// get carpark by carpark number
 app.MapGet("/carpark", async (CarparkInfoContext dbContext) =>
-{
-    var carparkRecord = await dbContext.CarparkInfos
-        .Where(x => x.CarparkNumber == requestCarparkNumber)
-        .ToListAsync();
-    return carparkRecord;
-});
+    {
+        var carparkRecord = await dbContext.CarparkInfos
+            .Where(x => x.CarparkNumber == requestCarparkNumber)
+            .ToListAsync();
+        return carparkRecord;
+    })
+    .WithName("GetCarpark")
+    .WithOpenApi();
 
 app.Run();
